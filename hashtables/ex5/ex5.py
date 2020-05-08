319f51408895bfb8566aa0b54e938b5eae6b1454
spencer-mcguire <smcguire1826@gmail.com>
2020-05-08 11:22:39 -0500

    first pass

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,11 +1,26 @@
 def finder(files, queries):
 
-    """
-    YOUR CODE HERE
-    """
+    # hash the queries as the key and the file path as the value for quick look up
+    cache = {}
+    results = []
 
-    return result
+    for q in queries:
+        for path in files:
+            if q in path:
+                cache[q] = path
+                files.remove(path)
+            
 
+    # print(cache)
+
+    # if my querie is in the cache, add the value to a list 
+
+    for q in queries:
+        if q in cache:
+            results.append(cache[q])
+        else:
+            continue
+    return results
 
 if __name__ == "__main__":
     files = [
